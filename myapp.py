2202e97ba915b2aecd989d7061934fe7486fbda6
Alhaj53 <eimamalhaj@gmail.com>
2026-04-13 12:20:34 +0000

    Refactor Flask routes and add home endpoint
    Removed the '/run' route and added a home route that returns a message.

--- myapp.py
+++ myapp.py
@@ -1,10 +1,12 @@
-from flask import Flask, request
+from flask import Flask
+import os
 
 app = Flask(__name__)
 
-@app.route("/run", methods=["POST"])
-def run():
-    print("تم تشغيل السكريبت")
-    return "تم التنفيذ"
+@app.route("/")
+def home():
+    return "موقعي شغال 🚀"
 
-app.run()
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 10000))
+    app.run(host="0.0.0.0", port=port)
