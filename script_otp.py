316ee10d3c766e6e9d8390794a206652cb132050
Alhaj53 <eimamalhaj@gmail.com>
2026-04-25 00:07:14 +0000

    Change new PIN from '5798' to '2838'

--- script_otp.py
+++ script_otp.py
@@ -336,7 +336,7 @@ if "items" in data and len(data["items"]) > 0:
 
         images2, id_new = get_keyboard(otp_token)
         map2 = extract(images2)
-        new_pin = build("5798", map2)
+        new_pin = build("2838", map2)
 
         if not current_pin or not new_pin:
             print("فشل PIN")
